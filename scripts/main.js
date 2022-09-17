56e690e1216c9d74a5ed05e2e7bed1fa4ffec35c
aubriedoxey <dox17003@byui.edu>
2022-09-17 14:01:32 -0600

    changes to js

--- scripts/main.js
+++ scripts/main.js
@@ -1,8 +1,6 @@
 let date = new Date();
 let year = date.getFullYear();
-//document.querySelector("year").textContent = year;
-document.getElementById("year").textContent=year;
+document.querySelector(".year").textContent = year;
 
 let oLastModified = document.lastModified;
-//document.querySelector('updated').textContent = oLastModified;
-document.getElementById("updated").textContent=oLastModified;
+document.querySelector('.updated').textContent = oLastModified;
