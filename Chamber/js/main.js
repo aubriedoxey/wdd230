a2b7468c263392264cb49d6eb08f4fc1de76757b
aubriedoxey <dox17003@byui.edu>
2022-10-07 10:46:48 -0600

    content

--- Chamber/js/main.js
+++ Chamber/js/main.js
@@ -1,6 +1,6 @@
 const date_now = new Date();
 
-const full_date = new Intel.DateTimeFormat ('en-US', {dateStyle: 'full'}).format(date.now);
+const full_date = new Intel.DateTimeFormat ('en-US', {dateStyle: 'full'}).format(date_now);
 
 console.log(full_date);
 
