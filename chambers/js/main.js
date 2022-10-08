a16e2f5510406c82ff9a8b9356637642ae74f692
aubriedoxey <dox17003@byui.edu>
2022-10-08 13:45:12 -0600

    everything

--- chambers/js/main.js
+++ chambers/js/main.js
@@ -1,8 +1,19 @@
 const date_now = new Date();
 
-const full_date = new Intel.DateTimeFormat ('en-US', {dateStyle: 'full'}).format(date_now);
+const full_date = new Intl.DateTimeFormat ('en-US', {dateStyle: 'full'}).format(date_now);
+
+document.querySelector('.date').textContent = full_date;
 
 console.log(full_date);
 
 //click event
+function toggleMenu() {
+    document.getElementsByClassName("navigation")[0].classlist.toggle("responsive");
+}
 
+let date = new Date();
+let year = date.getFullYear();
+document.querySelector(".year").textContent = year;
+
+let oLastModified = document.lastModified;
+document.querySelector('.updated').textContent = oLastModified;
