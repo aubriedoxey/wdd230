3897cd99687b63c605c2f3cddf7544a4e83b8e54
aubriedoxey <dox17003@byui.edu>
2022-10-21 11:10:50 -0600

    pics

--- chambers/js/main.js
+++ chambers/js/main.js
@@ -21,3 +21,15 @@ document.querySelector('.updated').textContent = oLastModified;
 if (date.getDay() == 1 || date.getDay() == 2) {
     document.querySelector('#banner').style.display = 'block';
 }
+
+//weather updates
+
+let temp = document.querySelector('#t').textContent;
+let windspeed = document.querySelector('#ws').textContent;
+let chill = Math.round((35.74 + (0.6215 * temp))-(35.75 * Math.pow(windspeed,0.16)) + (0.4275*temp*Math.pow(windspeed,0.16)));
+if (temp < 50 & windspeed > 3) {
+    document.querySelector('#wc').textContent = chill;
+} 
+    else {
+        chill='N/A';
+    }
