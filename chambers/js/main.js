80cd37defe3a090c887d3b49fc280c863dcb8016
aubriedoxey <dox17003@byui.edu>
2022-11-16 11:02:10 -0700

    updates weather

--- chambers/js/main.js
+++ chambers/js/main.js
@@ -34,6 +34,22 @@ if (temp < 50 & windspeed > 3) {
         chill='N/A';
     }
 
+const apiURL = "https://api.openweathermap.org/data/2.5/weather?id=5351428&appid=9fa85cce9ff44eb89eb0511a9301b64f";
+const getWeather = async () => {
+    const response = await fetch(apiURL);
+    const jsObject = await response.json();
+    console.log(jsObject);
+    // °F = (K - 273.15)* 1.8000 + 32.00
+    document.querySelector('#current-temp').textContent = ((jsObject.main.temp-273.15)*1.8+32).toFixed(2);
+    const iconsrc= `https://openweathermap.org/img/w/${jsObject.weather[0].icon}.png`;
+    const desc = jsObject.weather[0].description;
+    document.querySelector('#icon-src').textContent = iconsrc;
+    document.querySelector('#weathericon').setAttribute('src', iconsrc);
+    document.querySelector('#weathericon').setAttribute('alt', desc);
+    document.querySelector('figcaption').textContent = desc;
+  };
+getWeather();
+getWeather();
 //join form
 const feedbackElement = document.getElementById('feedback');
     const formElement = document.forms[0];
--- lesson10/weather.js
+++ lesson10/weather.js
@@ -3,5 +3,13 @@ const getWeather = async () => {
     const response = await fetch(apiURL);
     const jsObject = await response.json();
     console.log(jsObject);
+    // °F = (K - 273.15)* 1.8000 + 32.00
+    document.querySelector('#current-temp').textContent = ((jsObject.main.temp-273.15)*1.8+32).toFixed(2);
+    const iconsrc= `https://openweathermap.org/img/w/${jsObject.weather[0].icon}.png`;
+    const desc = jsObject.weather[0].description;
+    document.querySelector('#icon-src').textContent = iconsrc;
+    document.querySelector('#weathericon').setAttribute('src', iconsrc);
+    document.querySelector('#weathericon').setAttribute('alt', desc);
+    document.querySelector('figcaption').textContent = desc;
   };
 getWeather();
